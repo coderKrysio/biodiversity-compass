6610c49fb6c01a2ffe293add6e019ca4499688c7
Anushree <anushreejaiswal.srn@gmail.com>
2023-12-30 20:48:14 +0530

    added listing component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { About } from "@/components/about";
 import { Hero } from "@/components/hero";
 import { Issues } from "@/components/issues";
+import { ListingIssues } from "@/components/lisiting-issues";
 import { Navbar } from "@/components/navbar";
+import { Services } from "@/components/services";
 
 export default function Home() {
     return (
@@ -10,6 +12,8 @@ export default function Home() {
 			<Hero />
 			<About />
 			<Issues />
+            <ListingIssues />
+            {/* <Services /> */}
         </main>
     );
 }
